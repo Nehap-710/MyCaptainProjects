b62b0a62beeea46121121200d504d12da2354907
Nehap-710 <66563258+Nehap-710@users.noreply.github.com>
2020-06-19 12:17:25 +0530

    This file contains the assignment under topic loops
    this contains a python code for removing negative numbers from a list

---
+++ positivelist.py
@@ -0,0 +1,17 @@
+list1 = [12,-7,5,64,-14]
+num = list()
+for i in list1:
+    if i > 0:
+        num.append(i)
+
+print("list1:",list1)
+print(num)
+
+list2 = [12, 14,-95, 3]
+num1 = list()
+for j in list2:
+    if j > 0:
+        num1.append(j)
+
+print("list2:",list2)
+print(num1)
